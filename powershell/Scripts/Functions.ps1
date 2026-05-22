667e97c176b7747e25a3492a1edd48be7d15d49e
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2026-05-22 12:37:51 -0400

    feat(powershell): add head and tail functions

--- powershell/Scripts/Functions.ps1
+++ powershell/Scripts/Functions.ps1
@@ -4,3 +4,21 @@ function which($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue | 
     Select-Object -ExpandProperty Source -ErrorAction SilentlyContinue
 }
+
+function head {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+        [int]$n = 10
+    )
+    Get-Content $Path | Select-Object -First $n
+}
+
+function tail {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+        [int]$n = 10
+    )
+    Get-Content $Path | Select-Object -Last $n
+}
